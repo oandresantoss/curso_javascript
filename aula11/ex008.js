b0a5bb97e0943f1c616291f15e7f580a5c60885b
André Santos <andremissional@gmail.com>
2025-04-17 10:14:16 -0300

    exercio008.js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+let vel = 85
+console.log(`A velocidade do seu carro é ${vel}km/h`) 
+if (vel > 60) {
+    console.log ('Você ultrapassou o limite de velocidade: MULTADO')
+}
+console.log('Dirija sempre usando cinto de segurança!')

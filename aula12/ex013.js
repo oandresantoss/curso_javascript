134996bdccfa385dfd8884048f492310710d814e
André Santos <andremissional@gmail.com>
2025-04-17 14:25:35 -0300

    Update ex013.js

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -3,10 +3,10 @@ let diasema = agora.getDay()
 
 /*
   0 - Domingo
-  1 -Segunda
-  2- Terça
-  3- Quarta
-  4- Quinta
+  1 - Segunda
+  2 - Terça
+  3 - Quarta
+  4 - Quinta
   5 - Sexta
   6 - Sábado
 */

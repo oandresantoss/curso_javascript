73a1b20c25d2064d0032038a34e2af5399a000f5
André Santos <andremissional@gmail.com>
2025-04-17 13:27:32 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,8 +1,10 @@
-let idade = 17
+let idade = 35
 if (idade < 16) {
     console.log('NÃO VOTA')
 } else if (idade < 18) {
     console.log('Voto opcional')
+} else {
+    console.log('VOTO OBRIGATÓRIO')
 }
 
 

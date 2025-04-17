f4f50bf527358c4d18726e04cafc52ad4fc1fdeb
André Santos <andremissional@gmail.com>
2025-04-17 14:24:29 -0300

    Update ex013.js

---
+++ aula12/ex013.js
@@ -0,0 +1,39 @@
+let agora = new Date()
+let diasema = agora.getDay()
+
+/*
+  0 - Domingo
+  1 -Segunda
+  2- Terça
+  3- Quarta
+  4- Quinta
+  5 - Sexta
+  6 - Sábado
+*/
+//console.log(disasema)
+
+switch(diasema) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1: 
+        console.log('Segunda-Feira')
+        break
+    case 2:
+        console.log('Teça-Feira')
+    case 3:
+        console.log('Quarta-Feira')
+        break
+    case 4:
+        console.log('Quinta-Feira')
+        break
+    case 5:
+        console.log('Sexta-Feira')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('[ERRO] dia inválido')
+        break
+}

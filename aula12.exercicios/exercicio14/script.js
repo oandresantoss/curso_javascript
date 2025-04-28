960f611306decafd701235be8ae51a4fb3fd9cf3
André Santos <andremissional@gmail.com>
2025-04-27 23:39:02 -0300

    Update script.js

--- aula12.exercicios/exercicio14/script.js
+++ aula12.exercicios/exercicio14/script.js
@@ -4,4 +4,14 @@ let img = document.getElementById('imagem')
 let data = new Date()
 let hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'img/manha.jpg'
+    } else if (hora >=12 && hora < 18){
+        //Boa tarde
+          img.src = 'img/tarde.jpg'
+    }else {
+        //Boa noite
+          img.src = 'img/noite.jpg'
+    }
 }

c9591ecd5c463d0964a513cc481e69ee6430db1b
André Santos <andremissional@gmail.com>
2025-04-27 23:23:31 -0300

    Update script.js

--- aula12.exercicios/exercicio14/script.js
+++ aula12.exercicios/exercicio14/script.js
@@ -1,1 +1,7 @@
-let msg =
+function carregar(){
+let msg = document.getElementById('msg')
+let img = document.getElementById('imagem')
+let data = new Date()
+let hira = data.getHours()
+msg.innerHTML =
+}
